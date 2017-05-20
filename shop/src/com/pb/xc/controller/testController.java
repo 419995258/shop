7ff5871e91e4fd86c8197c779bdb01ff0869d41c
PB <419995258@qq.com>
2017-05-20 08:37:04 +0800

    1

--- shop/src/com/pb/xc/controller/testController.java
+++ shop/src/com/pb/xc/controller/testController.java
@@ -6,7 +6,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONObject;
+
+
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonArray;
 import com.pb.xc.controller.vo.Message;
 import com.pb.xc.entity.Student;
 
@@ -32,5 +36,24 @@ public class testController {
         result.put("seco", 132);
         message.setResult(result);
         return message;
+    }
+    
+    @RequestMapping(value = "/test2", method = RequestMethod.POST)
+    public JSONObject deleteGroup2() throws Exception {
+    	JSONArray array = new JSONArray();
+    	JSONObject json = null;
+    	json = new JSONObject();
+    	json.put("name", "a");
+    	json.put("age", 15);
+    	json.put("sex", "男");
+    	array.add(json);
+    	json = new JSONObject();
+    	json.put("name", "b");
+    	json.put("age", 14);
+    	json.put("sex", "女");
+    	array.add(json);
+    	JSONObject json2 = new JSONObject();
+    	json2.put("students", array);
+        return json2;
     }
 }
